9c6b42752ad127d485c3b9e59f2776d136d2b60f
Daewoong Ko <kodw4284@gmail.com>
2022-12-17 17:47:27 +0900

    feat: set up styled-nomalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MainPage from 'pages/main';
-
-import 'index.css';
+import GlobalStyle from 'global-styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <MainPage />
   </React.StrictMode>
 );
