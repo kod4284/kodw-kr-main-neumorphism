d5b457314ad6ddd03b484c073559e6e2735d4a69
Daewoong Ko <kodw4284@gmail.com>
2023-12-08 21:51:22 +0900

    chore: change ddns

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -8,12 +8,12 @@ import { NeuBoxOption } from './types';
 const newBoxOptions: Array<NeuBoxOption> = [
   { name: "Me", url: "https://kodw.netlify.app/", primary: true },
   { name: "B" , url: "https://books.kodw.kr"},
-  { name: "G" , url: "https://kodw0402.synology.me/"},
+  { name: "G" , url: "https://kodw.synology.me/"},
   { name: "N" , url: "https://npm.kodw.kr/"},
   { name: "Pa", url: "https://pass.kodw.kr/"},
   { name: "Ph", url: "https://photos.kodw.kr/"},
   { name: "Po", url: "https://portainer.kodw.kr/"},
-  { name: "R" , url: "http://kodw0402.synology.me:3012"},
+  { name: "R" , url: "http://kodw.synology.me:3012"},
   { name: "S" , url: "https://nas.kodw.kr/"},
 ];
 
